9d80e758fa724507daa0619a12548be9d0a06fe7
zhenxianyimeng <1920405993@qq.com>
2018-04-20 10:26:49 +0800

    ajax get maxscore

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,2 +1,28 @@
 var local = new Local();
 local.start();
+// window.onload = function() {
+//     $.get("/api/user",function(data,status){
+//         alert("Data: " + data + "\nStatus: " + status);
+//     });
+// };
+
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "/api/user", //路径
+        dataType: "json",
+        success: function (data) {
+            // alert(data.maxScore);
+            $("#max").text(data.maxScore);
+
+            // var music = "<h2>";
+            // //i表示在data中的索引位置，n表示包含的信息的对象
+            // $.each(data, function (i, n) {
+            //     //获取对象中属性为optionsValue的值
+            //     music += "<div>" + n["optionValue"] + "</div>";
+            // });
+            // music += "</h2>";
+            // $('#result').append(music);
+        }
+    });
+});
